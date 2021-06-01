4af053ff596f3c732335451241d4c533e28ee9bd
Jorge Vicuña <jvicuna2001@gmail.com>
2021-05-31 23:06:38 -0400

    Añadida capacidad de tener prefijos por servidor, enviar mensajes a través del bot, ver latencia del bot

--- bot.py
+++ bot.py
@@ -17,7 +17,6 @@ console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(console_handler)
 
-prefix = config.prefix
 base_modules = [
             'cogs.fun.hello',
             'cogs.fun.animal_pics',
@@ -25,14 +24,22 @@ base_modules = [
             'cogs.fun.kanye',
             'cogs.fun.games',
             'cogs.fun.tyaas',
+            'cogs.admin.misc',
             'cogs.admin.base_error_catch',
             'cogs.db.initialize'
             ]
             
+def get_prefix(bot, message:discord.Message):
+    file = open("prefixes.json", 'r')
+    prefixes = json.load(file)
+    file.close()
+    try:
+        return commands.when_mentioned_or(prefixes[f'{message.guild.id}'])(bot, message)
+    except (KeyError, AttributeError):
+        return commands.when_mentioned_or(config.prefix)(bot, message)
 
-
-bot = commands.Bot(command_prefix=prefix, intents=Intents.all())
-logger.info(f"Bot instance created with prefix {prefix}")
+bot = commands.Bot(command_prefix=get_prefix, intents=Intents.all())
+logger.info(f"Bot instance created with default prefix {config.prefix}")
 
 for module in base_modules:
     bot.load_extension(module)
